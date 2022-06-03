7d360fc99a28e5812b054dacea7f9e524c48b360
cmanor <101116379+cmanor@users.noreply.github.com>
2022-06-03 01:31:25 -0400

    I long for the sweet release of death

---
+++ src/main/java/com/example/springbootandoauth2/SpringBootAndOAuth2Application.java
@@ -0,0 +1,5 @@
+package com.example.springbootandoauth2;
+
+public class SpringBootAndOAuth2Application {
+    public static void main(String[] args){}
+}
---
+++ src/test/java/com/example/springbootandoauth2/SpringBootAndOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootandoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndOAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
